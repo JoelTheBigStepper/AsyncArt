b4393761c0e67d8b3df3a6c5ecc317d9a7119b55
Joel <caesarthebigstepper@gmail.com>
2026-06-11 22:21:46 +0100

    fuck me fr!!!!

--- portfolio-backend/server.js
+++ portfolio-backend/server.js
@@ -22,7 +22,7 @@ app.use(cors({
     "http://localhost:3000",
     "http://localhost:5173",
     "http://localhost:5174",
-    "https://velasturx.vercel.app",
+    "https://velastrux.vercel.app",
   ],
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
   allowedHeaders: ["Content-Type", "Authorization", "X-Velastrux-Token"],
